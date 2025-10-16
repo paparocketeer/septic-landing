09a52c2db6c6a6c2fef785f2f676d0cc1f8d8ae6
roman.mironovich <roman.mironovich@am-intech.ru>
2025-10-16 10:10:18 +0300

    fix build

--- app/products/[slug]/page.js
+++ app/products/[slug]/page.js
@@ -1,8 +1,15 @@
 import ProductDetailsPage from "@/app/_components/pages/ProductDetailsPage";
+import { products } from "@/app/_data/products";
 
 export const metadata = {
   title: "Product Details",
 };
+
+export async function generateStaticParams() {
+  return products.map((product) => ({
+    slug: product.slug,
+  }));
+}
 
 function Page() {
   return <ProductDetailsPage />;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
